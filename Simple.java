c6b9eab63d3e6185874f7b82149e80a4f5e0fd71
Devloper1 <developer1@gmail.com>
2023-01-04 02:49:37 +0530

    C4

--- Simple.java
+++ Simple.java
@@ -1,7 +1,7 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello Java ");
-     System.out.println("Hello Java Vikash");
+     System.out.println("Hello Java Vikash Kumar");
   
     }  
 }  
