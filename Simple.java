89487b4b35a4552984088a2970e3324fbee1fe3a
Devloper1 <developer1@gmail.com>
2023-01-04 22:33:24 +0530

    C5

--- Simple.java
+++ Simple.java
@@ -2,6 +2,7 @@ class Simple{
     public static void main(String args[]){  
      System.out.println("Hello Java ");
      System.out.println("Hello Java Vikash Kumar");
+     System.out.println("Git working successfully");
   
     }  
 }  
